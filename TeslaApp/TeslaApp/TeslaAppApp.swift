bc407e7b9ac49ea1a0e7efa7e57c4527af484f27
Vaka37 <vak-forever@inbox.ru>
2024-05-13 15:42:24 +0300

    app init

---
+++ TeslaApp/TeslaApp/TeslaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaAppApp.swift
+//  TeslaApp
+//
+//  Created by Vakil on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TeslaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
